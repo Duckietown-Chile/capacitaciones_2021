4fda5cef2f0566ecd0ecaf83359a6331f7da111e
Lukas Pavez <lukas.pavez@ug.uchile.cl>
2021-09-29 20:52:23 -0300

    add cap5 template

---
+++ cap5/README.md
@@ -0,0 +1,33 @@
+# Capacitación 4: Visión 3D
+
+Durante la capacitación 5 se mostraron los conceptos de localizacion que se utilizan en la robótica móvil.
+
+La misión para esta capacitación es localizarse dentro de un area. Para esto, deben utilizar el valor de f encontrado de manera experimental en el desafio anterior (freno de emergencia). 
+Tambien se debe usar la libreria `pupil_apriltags`, que se utiliza para identificar los apriltags dentro del mapa.
+Con la deteccion del apriltag, deben calcular la distancia del robot hasta el tag para corroborar que se encuentran dentro del area detectada.
+
+Se agrega el archivo `localize.py`, el cual posee la estructura principal y funciones de utilidad con sus respectivos comentarios que servirán para poder completar la misión.
+
+## Pasos previos
+Se debe instalar la libreria apriltag, para eso, deben seguir los siguientes pasos:
+```bash
+conda activate gym-duckietown
+pip install pupil-apriltags
+```
+
+## Funciones útiles
+Estas funciones serán de utilidad para resolver el desafío:
+
+
+`tags = at_detector.detect(gray_image, estimate_tag_pose=False, camera_params=None, tag_size=None)`: detecta los tags presentes en gray_image y los guarda en una lista de tags.
+
+`tag.corners`: obtiene una matriz con las coordenadas de las cuatro esquinas del tag.
+
+`tag.tag_id`: Obtiene la id correspondiente al tag detectado.
+
+## Mapa para calibración
+Además, se adjunta mapa (archivo `4tags.yaml`) que debe copiarse en la carpeta de mapas de `gym-duckietown`. Este mapa contiene 4 tags en cada esquina, el tamaño del cuadro que corresponde al tag es conocido (0.042 [m]), cada _tile_ mide 1 [m] y se pueden mover libremente por todo el espacio.
+
+El código se corre de la siguiente forma:
+
+`python localize.py`
---
+++ cap5/localize.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+"""
+Este programa implementa un freno de emergencia para evitar accidentes en Duckietown.
+"""
+
+import sys
+import argparse
+import gym
+import gym_duckietown
+from gym_duckietown.envs import DuckietownEnv
+import numpy as np
+import cv2
+from pupil_apriltags import Detector
+
+def mov_duckiebot(key):
+    # La acción de Duckiebot consiste en dos valores:
+    # velocidad lineal y velocidad de giro
+    actions = {ord('w'): np.array([1.0, 0.0]),
+               ord('s'): np.array([-1.0, 0.0]),
+               ord('a'): np.array([0.0, 1.0]),
+               ord('d'): np.array([0.0, -1.0]),
+               ord('q'): np.array([0.3, 1.0]),
+               ord('e'): np.array([0.3, -1.0])
+               }
+
+    action = actions.get(key, np.array([0.0, 0.0]))
+    return action
+
+
+if __name__ == '__main__':
+
+    # Se leen los argumentos de entrada
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env-name', default="Duckietown-udem1-v1")
+    parser.add_argument('--map-name', default='4tags')
+    args = parser.parse_args()
+
+    # Definición del environment
+    if args.env_name and args.env_name.find('Duckietown') != -1:
+        env = DuckietownEnv(
+            map_name = args.map_name,
+            domain_rand = False,
+        )
+    else:
+        env = gym.make(args.env_name)
+
+    # Se reinicia el environment
+    env.reset()
+
+    # se inicializa el detector
+    at_detector = Detector(families='tag36h11',
+                            nthreads=1,
+                            quad_decimate=1.0,
+                            quad_sigma=0.0,
+                            refine_edges=1,
+                            decode_sharpening=0.25,
+                            debug=0)
+
+    # valor de f encontrado en el desafio anterior
+    f = 1
+    
+    h = 0.042
+    area = -1
+
+    while True:
+
+        # Captura la tecla que está siendo apretada y almacena su valor en key
+        key = cv2.waitKey(0)
+        # Si la tecla es Esc, se sale del loop y termina el programa
+        if key == 27:
+            break
+
+        # Se define la acción dada la tecla presionada
+        action = mov_duckiebot(key)
+
+
+        # Se ejecuta la acción definida anteriormente y se retorna la observación (obs),
+        # la evaluación (reward), etc
+        obs, reward, done, info = env.step(action)
+        if done:
+            env.reset()
+
+        # convertir imagen a escala de grises
+
+        # detectar los tags
+        tags = []
+        for tag in tags:
+            # obtener esquinas del tag
+
+            # calcular p usando las esquinas encontradas (la altura de la deteccion)
+            p = 0
+
+            # calcular la distancia desde el robot hasta la deteccion
+            dist = 0
+
+            if dist <= 1:
+                #si se esta cerca del tag, asignar su id a la variable area
+                pass
+
+            #dibujar la deteccion en la imagen
+            
+            
+        print(f"ubicacion actual: area {area}")
+        cv2.imshow('patos', cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
+
+    # Se cierra el environment y termina el programa
+    env.close()
